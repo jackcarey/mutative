198acb00ab4120bc75517673a36f0b4e560f75cb
jackcarey <35701383+jackcarey@users.noreply.github.com>
2023-05-08 08:42:55 +0100

    initial

---
+++ README.md
@@ -0,0 +1,30 @@
+# mutative
+
+Inspired by [ArrowJS](https://www.arrow-js.com/)'s `reactive` function that converts regular data objects into observed data objects, this class aims to do the same with DOM elements so that DOM changes triggered by something other than ArrowJS can easily trigger events.
+
+## Use 
+
+Import the package by saving it locally or by using [jsDelivr](https://jsdelivr.com/) or [esh.sh](https://esh.sh/).
+
+```
+// local file
+import mutative from "./mutative.min.js";
+//jsDelivr
+import mutative from "https://cdn.jsdelivr.net/npm/@jackcarey/mutative@1.0.0/mutative.min.js"
+// jsdelivr (with ESM)
+import mutative from "https://cdn.jsdelivr.net/npm/@jackcarey/mutative/+esm"
+// esh.sh
+import mutative from "https://esm.sh/@jackcarey/mutative"
+
+//GitHub (latest version)
+import mutative from "https://cdn.jsdelivr.net/gh/jackcarey/mutative/mutative.min.js"
+
+```
+
+```
+```
+
+```
+```
+
+## How it works
---
+++ mutative.ts
@@ -0,0 +1,67 @@
+export default class mutative {
+    static #observerList: object;
+    static #mutationFn = (mutationList: MutationRecord[]) => {
+        Object.entries(mutative.#observerList).forEach(([selector, callback]) => {
+            mutationList.forEach((mutationRecord: MutationRecord) => {
+                [
+                    ...Array.from(mutationRecord?.addedNodes),
+                    ...Array.from(mutationRecord?.removedNodes),
+                    mutationRecord?.target,
+                ].forEach((el: Node) => {
+                    if (el instanceof Element && el.matches(selector)) {
+                        callback(mutationRecord);
+                    }
+                });
+            });
+        });
+    };
+    static #bodyObserver = new MutationObserver(mutative.#mutationFn);
+    selectorList: string[] = [];
+    constructor(selectorDict, callback) {
+        if (!mutative.#observerList) {
+            mutative.#observerList = {};
+            mutative.#bodyObserver.observe(document.body, {
+                attributes: true,
+                subtree: true,
+                childList: true,
+                characterData: true,
+                attributeOldValue: true,
+                characterDataOldValue: true,
+            });
+        }
+        const isString = typeof selectorDict === "string";
+        const isArray = Array.isArray(selectorDict);
+        if (!isString && !isArray && !(typeof selectorDict === "object")) {
+            throw new Error("selectorDict must be string, array, or object");
+        }
+        if (isString || isArray) {
+            if (typeof callback !== "function") {
+                throw new Error("callback must be a function");
+            }
+            (isArray ? selectorDict : [selectorDict]).forEach((query) => {
+                mutative.#observerList[query] == callback;
+                this.selectorList.push(query);
+            });
+        } else {
+            if (
+                Object.entries(selectorDict).some(
+                    ([key, fn]) => typeof key !== "string" || typeof fn !== "function"
+                )
+            ) {
+                throw new Error("All entries must be string-function pairs");
+            }
+            const obj = Object.create(mutative.#observerList);
+            mutative.#observerList = { obj, ...selectorDict };
+            this.selectorList = Object.keys(selectorDict);
+        }
+    }
+    disconnect = () => {
+        mutative.#mutationFn(mutative.#bodyObserver.takeRecords());
+        this.selectorList
+            .filter((k) => Object.keys(mutative.#observerList).includes(k))
+            .forEach((k) => {
+                delete mutative.#observerList[k];
+            });
+    };
+    toString = () => this.selectorList.join(", ");
+}
